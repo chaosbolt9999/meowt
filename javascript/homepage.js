7b9b6cc99c8a8a9f44fd3436016de27c19ebc4b5
racquelcatolico <56331306+racquelcatolico@users.noreply.github.com>
2020-01-01 10:18:16 +0800

    Add files via upload

---
+++ javascript/homepage.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'Stress. A common feeling of being overwhelmed, worried, or run-downed. Everyone deals with it, whether it be because of school, work, family, relationships, and many others. But now, we focus more on the stress that we students all deal with.';
+
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("start").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
