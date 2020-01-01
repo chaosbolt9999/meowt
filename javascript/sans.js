6d8aca8ecfc2b7a6d188f9fe70272c24fed03883
InternetPerson10 <58873475+InternetPerson10@users.noreply.github.com>
2020-01-01 16:05:29 +0800

    Create sans.js

---
+++ javascript/sans.js
@@ -0,0 +1,8 @@
+var contactUs = function() {
+			document.getElementById("boop").className += "beetlejuice";
+			document.getElementById("beep").className += "bestfriend";
+			document.getElementById("name").innerHTML = "";
+			document.getElementById("email").innerHTML = "";
+			document.getElementById("exp").innerHTML = "";
+			document.getElementById("bye").className += "beetlejuice";
+}
