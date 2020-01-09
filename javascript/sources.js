6aff9994f986fd5a24f9d1b8b9e5b8be4f32f2d6
chaosbolt9999 <56312534+chaosbolt9999@users.noreply.github.com>
2020-01-09 22:02:15 +0800

    Add files via upload

---
+++ javascript/sources.js
@@ -0,0 +1,6 @@
+function showsource(element_id){
+document.getElementById(element_id + 'src').style.visibility = 'visible';
+}
+function hidesource(element_id){
+document.getElementById(element_id + 'src').style.visibility = 'hidden';
+}
