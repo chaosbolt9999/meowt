11fcdfcc8a5c551f8eba3d6ef8612d13482ba6ea
InternetPerson10 <58873475+InternetPerson10@users.noreply.github.com>
2020-01-10 21:14:18 +0800

    Update sources.js

--- javascript/sources.js
+++ javascript/sources.js
@@ -1,6 +1,8 @@
 function showsource(element_id){
 document.getElementById(element_id + 'src').style.opacity = 0.8;
+document.getElementById(element_id + 'src').style.animation = "spaghettiUp ease-in-out 0.3s";
 }
 function hidesource(element_id){
 document.getElementById(element_id + 'src').style.opacity = 0;
+document.getElementById(element_id + 'src').style.animation = "spaghettiDown ease-in-out 0.3s";
 }
