0f64adf69fed22ea1890a38bd4dfc7cf20123b9a
chaosbolt9999 <56312534+chaosbolt9999@users.noreply.github.com>
2020-02-23 19:24:29 +0800

    Add files via upload

---
+++ javascript/phone.js
@@ -0,0 +1,93 @@
+
+var oneK = [
+  // keyframes
+{top: '0px'},
+{top: '78px',offset:0.3},
+{top: '78px', offeset: 1},
+]
+
+ var oneT = { 
+  // timing options
+  duration: 5000,
+  iterations: 1,
+}
+
+ var oneR = { 
+  // timing options
+  duration: 5000,
+  iterations: 1,
+  direction: 'reverse'
+}
+;
+var twoK = [
+  // keyframes
+{top: '10px'},
+{top: '89px',offset:0.3},
+{top: '89px'},
+]
+
+var twoT = { 
+  // timing options
+  duration: 5000,
+  iterations: 1,
+  
+};
+var twoR = { 
+  // timing options
+  duration: 5000,
+  iterations: 1,
+  direction: 'reverse'
+}
+var threeK = [
+  // keyframes
+{top: '10px'},
+{top: '91px',offset:0.3},
+{top: '91px' },
+]
+var threeT = { 
+  // timing options
+  duration: 5000,
+  iterations: 1,
+  
+};
+var threeR = { 
+  // timing options
+  duration: 5000,
+  iterations: 1,
+  direction: 'reverse'
+}
+function phone(){
+//animating
+
+document.getElementById("one").animate(
+oneK,
+oneT
+);
+setTimeout(function (){document.getElementById("one").animate(
+oneK,
+oneR
+)
+},5000);
+setTimeout(function(){document.getElementById("two").animate(
+twoK,
+twoT
+);
+setTimeout(function (){document.getElementById("two").animate(
+twoK,
+twoR
+)
+},5000);
+},10000);
+setTimeout(function(){document.getElementById("three").animate(
+threeK,
+threeT
+);
+setTimeout(function (){document.getElementById("three").animate(
+threeK,
+threeR
+)
+},5000);
+},20000);
+
+
+}
