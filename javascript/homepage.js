2e9c61fe3594bab125dee8b5e1a6ffebae6eff8c
InternetPerson10 <58873475+InternetPerson10@users.noreply.github.com>
2020-02-25 10:51:59 +0800

    Update homepage.js

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -2,7 +2,7 @@ var i = 0;
 var txt = 'Stress. A common feeling of being overwhelmed, worried, or run-downed. Everyone deals with it, whether it be because of school, work, family, relationships, and many others. But now, we focus more on the stress that we students all deal with.';
 var x = document.getElementsByClassName("button");
 var j;
-var speed = 30;
+var speed = 50;
 
 function hasClicked() {
   if(localStorage.getItem("visited")) {
@@ -17,10 +17,9 @@ function hasClicked() {
 
 function typeWriter() {
   localStorage.setItem("visited", "true");
-  document.getElementById("startbutton").style.animation = "spaghettiDown 0.3s ease-in-out";
-  setTimeout(typeWriter, 300);
-  document.getElementById("startbutton").style.opacity = 0;
-  document.getElementById("startbutton").style.display = "none";
+  if(i == 0) document.getElementById("startbutton").style.animation = "spaghettiDown 0.3s ease-in-out";
+  if(i == 6) document.getElementById("startbutton").style.opacity = 0;
+  if(i == 6) document.getElementById("startbutton").style.display = "none";
   if (i < txt.length) {
     document.getElementById("start").innerHTML += txt.charAt(i);
     i++;
