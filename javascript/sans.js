a88af13830163532a0af92c2ab8a2c72326ae52d
InternetPerson10 <58873475+InternetPerson10@users.noreply.github.com>
2020-02-27 08:00:09 +0800

    Update sans.js

--- javascript/sans.js
+++ javascript/sans.js
@@ -1,4 +1,18 @@
+var hasClicked = function() {
+	if(localStorage.getItem("visited")) {
+			document.getElementById("boop").className += "beetlejuice";
+			document.getElementById("beep").className += "bestfriend";
+			document.getElementById("name").innerHTML = "";
+			document.getElementById("email").innerHTML = "";
+			document.getElementById("exp").innerHTML = "";
+			document.getElementById("bye").className += "beetlejuice";
+	}
+}
+
+document.addEventListener('DOMContentLoaded', hasClicked, false);
+
 var contactUs = function() {
+			localStorage.setItem("visited", "true");
 			document.getElementById("boop").className += "beetlejuice";
 			document.getElementById("beep").className += "bestfriend";
 			document.getElementById("name").innerHTML = "";
